519f5daa2b1ab4baabe2c7b9947b37650c272b21
Valverde <96694746+Valverde-101@users.noreply.github.com>
2026-09-11 20:28:36 -0500

    feat(commands): expose command center data and results

--- src/client/views/commands/commands-preload.ts
+++ src/client/views/commands/commands-preload.ts
@@ -1,10 +1,15 @@
 import { addDispatchEventListeners } from '@common/utils';
 import { ipcRenderer } from 'electron';
 
-addDispatchEventListeners(['get-players'], ipcRenderer);
+addDispatchEventListeners([
+  'get-players',
+  'get-command-center-data',
+  'command-result'
+], ipcRenderer);
 
 (window as any).api = {
   fetchPlayers: () => ipcRenderer.send('get-players'),
+  fetchCommandCenterData: () => ipcRenderer.send('get-command-center-data'),
   openCommandsList: () => ipcRenderer.send('open-commands-list'),
   runCommand: (obj: any) => ipcRenderer.send('run-command', obj)
 };
