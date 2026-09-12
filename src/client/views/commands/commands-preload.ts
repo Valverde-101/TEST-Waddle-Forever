2586b749a39666f93532974c902c190f57ab0b8b
Valverde <96694746+Valverde-101@users.noreply.github.com>
2026-09-12 06:55:27 -0500

    fix(commands): expose request response IPC api

--- src/client/views/commands/commands-preload.ts
+++ src/client/views/commands/commands-preload.ts
@@ -1,15 +1,8 @@
-import { addDispatchEventListeners } from '@common/utils';
 import { ipcRenderer } from 'electron';
 
-addDispatchEventListeners([
-  'get-players',
-  'get-command-center-data',
-  'command-result'
-], ipcRenderer);
-
 (window as any).api = {
-  fetchPlayers: () => ipcRenderer.send('get-players'),
-  fetchCommandCenterData: () => ipcRenderer.send('get-command-center-data'),
+  fetchPlayers: () => ipcRenderer.invoke('command-center:get-players'),
+  fetchCommandCenterData: () => ipcRenderer.invoke('command-center:get-data'),
   openCommandsList: () => ipcRenderer.send('open-commands-list'),
-  runCommand: (obj: any) => ipcRenderer.send('run-command', obj)
+  runCommand: (obj: any) => ipcRenderer.invoke('command-center:run-command', obj)
 };
