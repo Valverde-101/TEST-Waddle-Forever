62d0ec27a308b86e0b46088e49138e2affcb1f07
Valverde <96694746+Valverde-101@users.noreply.github.com>
2026-09-18 08:04:39 -0500

    test(party): prove Mine Shack to School doorway

--- .github/scripts/waddle-halloween-2015-protocol.ps1
+++ .github/scripts/waddle-halloween-2015-protocol.ps1
@@ -182,6 +182,7 @@ $requiredPartyMethods = New-Object 'System.Collections.Generic.HashSet[string]' 
 $requiredPartyConstants = New-Object 'System.Collections.Generic.HashSet[string]' ([StringComparer]::Ordinal)
 $soloRoomEvidence = New-Object 'System.Collections.Generic.HashSet[string]' ([StringComparer]::OrdinalIgnoreCase)
 $coffeeSecretEntryEvidence = New-Object 'System.Collections.Generic.HashSet[string]' ([StringComparer]::OrdinalIgnoreCase)
+$schoolDoorEvidence = New-Object 'System.Collections.Generic.HashSet[string]' ([StringComparer]::OrdinalIgnoreCase)
 $finaleRoomEvidence = New-Object 'System.Collections.Generic.HashSet[string]' ([StringComparer]::OrdinalIgnoreCase)
 
 function Add-HalloweenRoomRuntimeContract($Evidence,[string]$Role) {
@@ -203,6 +204,11 @@ function Add-HalloweenRoomRuntimeContract($Evidence,[string]$Role) {
     if (($Role -match '(?i)coffee' -or $body -match '(?i)class\s+com\.clubpenguin\.world\.rooms2015\.october\.Coffee') -and
         $body -match '(?i)party1_mc' -and $body -match '(?i)enterCave') {
       [void]$coffeeSecretEntryEvidence.Add("$Role::party1_mc->enterCave")
+    }
+    if (($Role -match '(?i)robot-room-shack' -or $body -match '(?i)class\s+com\.clubpenguin\.world\.rooms2015\.october\.Shack') -and
+        $body -match '(?i)triggers_mc\.school_mc' -and
+        $body -match '(?i)(?:exit|sendJoinRoom)[^\r\n]{0,160}["'']school["'']') {
+      [void]$schoolDoorEvidence.Add("$Role::shack->school")
     }
     if ($Role -match '(?i)partysolo1' -and
         $body -match '(?i)PENULTIMATE_TASK_ID\s*=\s*8' -and
@@ -349,6 +355,7 @@ foreach ($method in @($requiredPartyMethods | Sort-Object)) { Write-Host "WADDLE
 foreach ($constant in @($requiredPartyConstants | Sort-Object)) { Write-Host "WADDLE_PARTY2015_RUNTIME_REQUIRED_CONSTANT=$constant" }
 foreach ($evidenceLine in @($soloRoomEvidence | Sort-Object)) { Write-Host "WADDLE_PARTY2015_SOLO_ROOM_EVIDENCE=$evidenceLine" }
 foreach ($evidenceLine in @($coffeeSecretEntryEvidence | Sort-Object)) { Write-Host "WADDLE_PARTY2015_COFFEE_SECRET_ENTRY=$evidenceLine" }
+foreach ($evidenceLine in @($schoolDoorEvidence | Sort-Object)) { Write-Host "WADDLE_PARTY2015_SCHOOL_DOOR=$evidenceLine" }
 foreach ($evidenceLine in @($finaleRoomEvidence | Sort-Object)) { Write-Host "WADDLE_PARTY2015_FINALE_ROOM=$evidenceLine" }
 if ($soloRoomEvidence.Count -lt 1) {
   throw 'WADDLE_PARTY2015_PROTOCOL=FAIL partysolo1_entry_contract_not_found'
@@ -356,10 +363,13 @@ if ($soloRoomEvidence.Count -lt 1) {
 if ($coffeeSecretEntryEvidence.Count -lt 1) {
   throw 'WADDLE_PARTY2015_PROTOCOL=FAIL coffee_secret_lair_entry_not_found'
 }
+if ($schoolDoorEvidence.Count -lt 1) {
+  throw 'WADDLE_PARTY2015_PROTOCOL=FAIL shack_school_door_contract_not_found'
+}
 if ($finaleRoomEvidence.Count -lt 1) {
   throw 'WADDLE_PARTY2015_PROTOCOL=FAIL finale_task8_to_task9_postdefeat_sequence_contract_not_found'
 }
-Write-Host "WADDLE_PARTY2015_RUNTIME_PARITY=PASS required_methods=$($requiredPartyMethods.Count) required_constants=$($requiredPartyConstants.Count) solo_room_evidence=$($soloRoomEvidence.Count) coffee_secret_entry=$($coffeeSecretEntryEvidence.Count) finale_room=$($finaleRoomEvidence.Count)"
+Write-Host "WADDLE_PARTY2015_RUNTIME_PARITY=PASS required_methods=$($requiredPartyMethods.Count) required_constants=$($requiredPartyConstants.Count) solo_room_evidence=$($soloRoomEvidence.Count) coffee_secret_entry=$($coffeeSecretEntryEvidence.Count) school_door=$($schoolDoorEvidence.Count) finale_room=$($finaleRoomEvidence.Count)"
 
 foreach ($target in $compatibilityTargets) {
   $swf = Join-Path $partyRoot ([string]$target.path)
